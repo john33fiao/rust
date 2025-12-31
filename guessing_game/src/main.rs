1043b80a0c07a656f8479be40995bd411a76d393
서요한 <john33fiao@gmail.com>
2025-12-31 13:59:02 +0900

    점심시간 공부

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,9 +8,18 @@ fn main() {
     let mut guess = String::new(); // 변경 가능한 변수는 mut을 쓴다
     // 러스트에서 변수는 기본적으로 불변을 가정함
 
+    // 얘는 io::stdin().readline(&mut guess).expect("잘못된 입력입니다."); 로도 표현 가능
     io::stdin()
-        .read_line(&mut guess)
-        .expect("잘못된 입력입니다.");
+        .read_line(&mut guess) // 임시저장 및 result 반환(열거형)
+        .expect("잘못된 입력입니다."); // Err 반환하면 경고문구 표시 후 종료
+        // expect는 Result 열거형의 메서드
+        // expect 안쓰면 컴파일러에서 경고 발생 (컴파일은 되지만)
     
-    println!("당신이 추측한 숫자: {guess}");
+    println!("당신이 추측한 숫자: {guess}"); // {}는 플레이스홀더, 변수 값 출력
+
+    // 자리표시자는 이렇게도 사용가능
+
+    let x = 5;
+    let y = 10;
+    println!("x = {x}, y = {}, x + y = {}", y, x + y); // 근데 보통 이렇게 두 개 이상이면 헷갈리니까 잘 안쓰는듯
 }
