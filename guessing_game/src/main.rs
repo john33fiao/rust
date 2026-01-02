778e6c370c574a850630d0b153b58ab3c651dcd3
john33fiao <john33fiao@gmail.com>
2026-01-02 22:16:30 +0900

    일일공부

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
 use std::io; // 표준 입출력 라이브러리
+use rand::Rng; // 난수 생성 라이브러리
 
 fn main() {
     println!("업다운 게임");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // 1부터 100까지 난수 생성
+
+    println!("비밀 숫자: {secret_number}"); // 디버깅용, 나중에 지울 것
 
     println!("1부터 100까지 숫자 중 하나를 맞춰보세요!");
 
@@ -19,7 +24,9 @@ fn main() {
 
     // 자리표시자는 이렇게도 사용가능
 
-    let x = 5;
-    let y = 10;
-    println!("x = {x}, y = {}, x + y = {}", y, x + y); // 근데 보통 이렇게 두 개 이상이면 헷갈리니까 잘 안쓰는듯
+    // println!("자리표시자 사용 예시:");
+
+    // let x = 5;
+    // let y = 10;
+    // println!("x = {x}, y = {}, x + y = {}", y, x + y); // 근데 보통 이렇게 두 개 이상이면 헷갈리니까 잘 안쓰는듯
 }
